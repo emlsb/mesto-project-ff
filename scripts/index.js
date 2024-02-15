aa1881a40a305c73626e58d2aa6cae98b695162c
Emil Sibiev <emlsb@gmail.com>
2024-02-15 11:20:38 +0300

    Добавил репозиторий

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.places__item card').cloneNode(true);
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-
+function addCard (image, description) {
+  cardElement.querySelector('.card__image') = image;
+  cardElement.querySelector('.card__title').textContent = description;
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
