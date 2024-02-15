7b148445c0884e4fdc3ed99327efef288ab5584a
Emil Sibiev <emlsb@gmail.com>
2024-02-15 19:23:08 +0300

    Вывел карточки на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,19 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const cardElement = cardTemplate.querySelector('.places__item card').cloneNode(true);
+
+
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
-function addCard (image, description) {
-  cardElement.querySelector('.card__image') = image;
-  cardElement.querySelector('.card__title').textContent = description;
-}
+initialCards.forEach(function(elem) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = elem.link
+  cardElement.querySelector('.card__title').textContent = elem.name
+  cardList.append(cardElement)
+})
+
+
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
