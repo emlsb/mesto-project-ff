defc7287b2367968497beeae311e743cb8e464c0
Emil Sibiev <emlsb@gmail.com>
2024-02-15 22:12:44 +0300

    переделал функцию addcard

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
-initialCards.forEach(function(elem) {
+function addCard (image, title, delButton='') {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = elem.link
-  cardElement.querySelector('.card__title').textContent = elem.name
+  cardElement.querySelector('.card__image').src = image;
+  cardElement.querySelector('.card__title').textContent = title;
+
   cardList.append(cardElement)
+}
+initialCards.forEach(function(elem) {
+  addCard(elem.link, elem.name)
 })
 
 
