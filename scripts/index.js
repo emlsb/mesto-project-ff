4d0d595f5aece1f1c0c8537e81fc0ca671071d6a
Emil Sibiev <emlsb@gmail.com>
2024-02-15 23:12:54 +0300

    Остановился на написании функции удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,24 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
+
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list')
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard (image, title, delButton='') {
+function addCard (image, title) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = image;
+  const cardImage = cardElement.querySelector('.card__image');
   cardElement.querySelector('.card__title').textContent = title;
+  cardImage.src = image;
+  cardImage.alt = title
 
   cardList.append(cardElement)
-}
-initialCards.forEach(function(elem) {
-  addCard(elem.link, elem.name)
-})
-
+};
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(elem) {
+  addCard(elem.link, elem.name)
+});
